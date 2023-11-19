44c2adc1ec909522e5c9419ddfba41207a81b2d5
dpranavsaravanan <pranavsaravanan2001@gmail.com>
2023-11-19 06:42:44 +0530

    A Clone of Instagram, built using React

---
+++ instafrontend/src/data/myinfo.js
@@ -0,0 +1,62 @@
+let myInfo  = {
+    profilePic:"https://img.freepik.com/premium-photo/very-beautiful-scenery-village-morning_80375-596.jpg?w=826",
+    username:"pranav106032",
+    post:45,
+    followers:265,
+    following:453,
+    name:"Pranav Saravanan",
+    Bio:"Software Developer",
+    savedStatus:[
+        {
+            statusName:"Songs",
+            status:[
+                "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg",
+                "https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=",
+                "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg",
+                "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
+            ]
+        },
+        {
+            statusName:"sketch",
+            status:[
+                "https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=",
+                "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg",
+                "https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=",
+                "https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=",
+                "https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=",
+                "https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0="
+            ]
+        },
+        
+    ],
+    posts:[
+        {
+            postImages:[
+                "https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?size=626&ext=jpg&ga=GA1.1.867424154.1698192000&semt=ais",
+                "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",]
+        },
+        {
+            postImages:[
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ79NESTMgtSRQKR5EjhZ32AaYOasSA1w2pA&usqp=CAU",
+                "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",]
+        },
+        {
+            postImages:[
+                "https://media.istockphoto.com/id/489343716/photo/humayuns-tomb-delhi-india-cngltrv1109.jpg?b=1&s=170667a&w=0&k=20&c=m2h3Ofb4zGEC82YxxjaxBkO--ZoWRL58X702RssCdUo=",
+                "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",]
+        },
+        {
+            postImages:[
+                "https://media.istockphoto.com/id/489343716/photo/humayuns-tomb-delhi-india-cngltrv1109.jpg?b=1&s=170667a&w=0&k=20&c=m2h3Ofb4zGEC82YxxjaxBkO--ZoWRL58X702RssCdUo=",
+                "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",]
+        },
+        {
+            postImages:[
+                "https://media.istockphoto.com/id/489343716/photo/humayuns-tomb-delhi-india-cngltrv1109.jpg?b=1&s=170667a&w=0&k=20&c=m2h3Ofb4zGEC82YxxjaxBkO--ZoWRL58X702RssCdUo=",
+                "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",]
+        },
+        
+    ]
+}
+
+export {myInfo};
---
+++ instafrontend/src/data/statusData.js
@@ -0,0 +1,42 @@
+let obj= [
+    {
+        username:"user1",
+        profilePic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7DLHpXkDMGV-d2iD4p_8mI9Uzk3m3zFQ3hfdWeAUqtw&s",
+        statusPic:[
+            {
+                imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5jiZJaV-KiJhWVPWg4MJKmq8-G7EKLjMORvHDg82&s"
+            },
+            {
+                imageUrl:"https://t4.ftcdn.net/jpg/05/50/61/75/360_F_550617518_Eu3U6kTl3mODPvnfTMFHQ88z1y5KNfNQ.jpg"
+            }
+        ]
+    },
+    {
+        username:"user_jksnfjks",
+        profilePic:"https://cdn-icons-png.flaticon.com/512/219/219969.png",
+        statusPic:[
+            {
+                imageUrl:"https://t4.ftcdn.net/jpg/05/50/61/75/360_F_550617518_Eu3U6kTl3mODPvnfTMFHQ88z1y5KNfNQ.jpg"
+            },
+            {
+                imageUrl:"https://t4.ftcdn.net/jpg/05/50/61/75/360_F_550617518_Eu3U6kTl3mODPvnfTMFHQ88z1y5KNfNQ.jpg"
+            }
+        ]
+    },
+    
+    {
+        username:"tina",
+        profilePic:"https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png",
+        statusPic:[
+            {
+                imageUrl:"https://cdna.artstation.com/p/media_assets/images/images/001/084/236/large/How-to-Draw-Easy-Scenery-of-Sunset-in-the-Hills-1024x576.jpg?1682356956"
+            },
+            {
+                imageUrl:"https://t4.ftcdn.net/jpg/05/50/61/75/360_F_550617518_Eu3U6kTl3mODPvnfTMFHQ88z1y5KNfNQ.jpg"
+            }
+        ]
+    },
+    
+]
+
+export {obj};
